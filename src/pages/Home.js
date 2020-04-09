1e94072d59d2fd10ec7b58fa6af437b7a919baeb
Yiqi Zhang <>
2020-04-09 17:23:00 -0400

    stuck on passing function

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,17 @@ import axios from "axios";
 const Home = () => {
   const [applePrice, setApplePrice] = useState(0);
   const [originalApplePrice, setOriginalApplePrice] = useState(0);
+  const [phoneCost, setPhoneCost] = useState();
+  const [shares, setShares] = useState(0);
+
+  const handlePhoneChange = (event) => {
+    setPhoneCost(event.target.value);
+  };
+
+  console.log("TEST", setShares);
+  setShares(function () {
+    return phoneCost / originalApplePrice;
+  });
 
   axios
     .get("https://financialmodelingprep.com/api/v3/company/profile/AAPL")
@@ -28,12 +39,16 @@ const Home = () => {
   return (
     <>
       <div style={{ display: "flex", flexDirection: "column" }}>
-        <div>Cost: 1000</div>
+        <input
+          placeholder="cost"
+          value={phoneCost}
+          onChange={handlePhoneChange}
+        />
         <div>Current Price: {applePrice}</div>
         <div>Original Price: {originalApplePrice}</div>
-        <div>Could have bought: {1000 / originalApplePrice} Shares</div>
+        <div>Could have bought: {shares} Shares</div>
         <div>Now worth: {4.444 * applePrice} </div>
-        <div>Net Potential Gain(Loss): {1191 - 1000} </div>
+        <div>Net Potential Gain(Loss): {1191 - phoneCost} </div>
       </div>
     </>
   );
