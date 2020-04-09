888681ca2427c1d27b2e409b9a6c3ad4a3e17d14
Yiqi Zhang <>
2020-04-09 16:48:06 -0400

    hard coded iphone walkthrough

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,7 @@ import axios from "axios";
 
 const Home = () => {
   const [applePrice, setApplePrice] = useState(0);
-
-  console.log("apple", applePrice);
-  console.log("apple2", setApplePrice);
+  const [originalApplePrice, setOriginalApplePrice] = useState(0);
 
   axios
     .get("https://financialmodelingprep.com/api/v3/company/profile/AAPL")
@@ -17,9 +15,26 @@ const Home = () => {
       console.log(error);
     });
 
+  axios
+    .get("https://financialmodelingprep.com/api/v3/enterprise-value/AAPL")
+    .then((response) => {
+      console.log(response.data.enterpriseValues[1]["Stock Price"]);
+      setOriginalApplePrice(response.data.enterpriseValues[1]["Stock Price"]);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
   return (
     <>
-      <div>{applePrice}</div>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div>Cost: 1000</div>
+        <div>Current Price: {applePrice}</div>
+        <div>Original Price: {originalApplePrice}</div>
+        <div>Could have bought: {1000 / originalApplePrice} Shares</div>
+        <div>Now worth: {4.444 * applePrice} </div>
+        <div>Net Potential Gain(Loss): {1191 - 1000} </div>
+      </div>
     </>
   );
 };
