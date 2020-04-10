0013ee8656e8e36fdd3600c593621f6f8d094ea8
Yiqi Zhang <>
2020-04-09 21:06:04 -0400

    submit dynamic calculation good

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,17 +4,30 @@ import axios from "axios";
 const Home = () => {
   const [applePrice, setApplePrice] = useState(0);
   const [originalApplePrice, setOriginalApplePrice] = useState(0);
-  const [phoneCost, setPhoneCost] = useState();
+  const [phoneCost, setPhoneCost] = useState(0);
+  const [yearBought, setYearBought] = useState(0);
   const [shares, setShares] = useState(0);
+  const [currentValue, setCurrentValue] = useState(0);
+  const [gainLoss, setGainLoss] = useState(0);
 
   const handlePhoneChange = (event) => {
     setPhoneCost(event.target.value);
   };
 
-  console.log("TEST", setShares);
-  setShares(function () {
-    return phoneCost / originalApplePrice;
-  });
+  const handleYearChange = (event) => {
+    setYearBought(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    let sharesBought = (phoneCost / originalApplePrice).toFixed(2);
+    let currentWorth = (sharesBought * applePrice).toFixed(2);
+    let currentGain = (currentWorth - phoneCost).toFixed(2);
+
+    setCurrentValue(currentWorth);
+    setShares(sharesBought);
+    setGainLoss(currentGain);
+    event.preventDefault();
+  };
 
   axios
     .get("https://financialmodelingprep.com/api/v3/company/profile/AAPL")
@@ -39,16 +52,26 @@ const Home = () => {
   return (
     <>
       <div style={{ display: "flex", flexDirection: "column" }}>
-        <input
-          placeholder="cost"
-          value={phoneCost}
-          onChange={handlePhoneChange}
-        />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="number"
+            placeholder="year"
+            value={yearBought}
+            onChange={handleYearChange}
+          />
+          <input
+            type="number"
+            placeholder="cost"
+            value={phoneCost}
+            onChange={handlePhoneChange}
+          />
+          <input type="submit" value="Submit" />
+        </form>
         <div>Current Price: {applePrice}</div>
         <div>Original Price: {originalApplePrice}</div>
         <div>Could have bought: {shares} Shares</div>
-        <div>Now worth: {4.444 * applePrice} </div>
-        <div>Net Potential Gain(Loss): {1191 - phoneCost} </div>
+        <div>Now worth: {currentValue} </div>
+        <div>Net Potential Gain(Loss): {gainLoss} </div>
       </div>
     </>
   );
---
+++ src/pages/timApple.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+function getComparison(props) {
+  const [phoneCost, setPhoneCost] = useState();
+
+  return (
+    <form>
+      <input type="submit" value={SetPhoneCost}></input>
+      <input type="text"></input>
+    </form>
+  );
+}
+
+export default getComparison;
