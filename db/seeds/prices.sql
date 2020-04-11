ab6a9fa76d1273a48429ea40062ab193420f511b
Flavian <flavian.mohanraj@gmail.com>
2020-04-11 13:54:18 -0400

    google api working

---
+++ db/seeds/prices.sql
@@ -0,0 +1,21 @@
+INSERT INTO phones (id)
+values 
+(1, apple),
+(2, google),
+(3, samsung);
+
+INSERT INTO versions (phones_id, name_id, year_id, price_id )
+values
+(1, iphone8, 2017, 1000),
+(1, iphone8, 2018, 800),
+(1, iphone8, 2019, 500),
+(1, iphone8, 2020, 300),
+
+
+SElect iphone8 where year_id === the dropdown
+
+
+(1, iphone8, 2017, 1000),
+(1, iphone8, 2017, 1000),
+(1, iphone8, 2017, 1000),
+
